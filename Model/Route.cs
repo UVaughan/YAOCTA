06d5d271476a1f9f42e7e0311119e5e8185a81c6
ulysses vaughan <uvaug070@uottawa.ca>
2019-09-05 00:27:26 -0400

    added trips

--- Model/Route.cs
+++ Model/Route.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using YAOCTA.Utility;
 
 namespace YAOCTA.Model
 {
@@ -11,5 +13,8 @@ namespace YAOCTA.Model
         public int DirectionID { get; set; }
         public string Direction { get; set; }
         public string RouteHeading { get; set; }
+
+        [JsonConverter(typeof(SingleOrArrayConverter<Trip>))]
+        public List<Trip> Trips { get; set; }
     }
 }
---
+++ Model/Trip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YAOCTA.Model
+{
+    public class Trip
+    {
+        public string TripDestination { get; set; }
+        public string TripStartTime { get; set; }
+        public string AdjustedScheduleTime { get; set; }
+        public string AdjustmentAge { get; set; }
+        public bool LastTripOfSchedule { get; set; }
+        public string BusType { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string GPSSpeed { get; set; }
+    }
+}
