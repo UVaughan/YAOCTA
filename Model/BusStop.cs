f0a28a31ac95de3ff48545b9ea31d3dbd736ca3b
ulysses vaughan <uvaug070@uottawa.ca>
2019-09-04 22:18:34 -0400

    now uses real data

---
+++ Model/BusStop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YAOCTA.Model
+{
+    public class BusStop
+    {
+        public string StopNo { get; set; }
+        public string StopDescription { get; set; }
+        public string Error { get; set; }
+        public RouteWrapper Routes { get; set; }
+    }
+}
---
+++ Model/RouteSummaryForStopWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YAOCTA.Model
+{
+    public class RouteSummaryForStopWrapper
+    {
+        public BusStop GetRouteSummaryForStopResult { get; set; }
+    }
+}
