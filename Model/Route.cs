d8461bdc711065693cd352d7256f669de5dfec6e
ulysses vaughan <uvaug070@uottawa.ca>
2019-09-04 20:36:06 -0400

    added stop page with dummy data

---
+++ Model/Route.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YAOCTA.Model
+{
+    public class Route
+    {
+        public string RouteNo { get; set; }
+        public int DirectionID { get; set; }
+        public string Direction { get; set; }
+        public string RouteHeading { get; set; }
+    }
+}
