021e015d5604b3dd0a36d50e3466230308fc0e7f
ulysses vaughan <uvaug070@uottawa.ca>
2019-09-05 13:10:14 -0400

    improved UI

---
+++ Utility/StringManipulation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YAOCTA.Utility
+{
+    public class StringManipulation
+    {
+        public static string FormatStopName(string stopName)
+        {
+            var capitalized = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(stopName.ToLower());
+            var replacedSlashes = capitalized.Replace("/", "and");
+            return replacedSlashes;
+        }
+    }
+}
