f3e636245193540587f92b3fa6c1ba9a6efc003f
ulysses vaughan <uvaug070@uottawa.ca>
2019-09-04 22:59:53 -0400

    fixed case where bus stop has only one route

--- Model/RouteWrapper.cs
+++ Model/RouteWrapper.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using YAOCTA.Utility;
 
 namespace YAOCTA.Model
 {
     public class RouteWrapper
     {
-        public Route[] Route { get; set; }
+        [JsonConverter(typeof(SingleOrArrayConverter<Route>))]
+        public List<Route> Route { get; set; }
     }
 }
---
+++ Utility/SingleOrArrayConverter.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YAOCTA.Utility
+{
+    /// <summary>
+    /// Used to handle instances where a property can be single object or array when deserializing
+    /// Credit to https://stackoverflow.com/questions/18994685/how-to-handle-both-a-single-item-and-an-array-for-the-same-property-using-json-n
+    /// </summary>
+    /// <typeparam name="T">Type of property</typeparam>
+    public class SingleOrArrayConverter<T> : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(List<T>));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JToken token = JToken.Load(reader);
+            if (token.Type == JTokenType.Array)
+            {
+                return token.ToObject<List<T>>();
+            }
+            return new List<T> { token.ToObject<T>() };
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
